19390e81f0deaa476119a729091efb001eef5edd
sujcho <chosujin84@gmail.com>
2017-04-01 14:14:47 -0700

    ADD SERVER - NODE.JS
    - app: node.js server
    - need mongoDB, node.js installed

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+app.use('/api', require('./app/dispatcher'));
+
+mongoose.connection
+	.on('error', function(err) {console.log(err.message)})
+	.on('disconnected', function() {
+		console.log('Mongoose connnection to DB has been disconnected');
+	})
+	.on('connected', function() {
+		app.listen(process.env.PORT, process.env.IP);
+	});
+
+var closeDBConnection = function() {
+	mongoose.connection.close(function() { process.exit(0); });
+};
+process.on('SIGINT', closeDBConnection).on('SIGTERM', closeDBConnection);
+
+mongoose.connect(require('./config/db').connectString);
